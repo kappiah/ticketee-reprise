f735aa6576d072ace57289ba005929becf784920
Kwasi Appiah <kwasi@appiah.id.au>
2015-09-17 01:39:05 +1000

    Auth using clearance over devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def sign_in(user)
+    flash[:notice] = "You have signed in successfully"
+    super(user)
+  end
 end
---
+++ spec/features/auth_and_signup_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.feature "authentication and signup" do
+  let!(:user) { FactoryGirl.create(:user) }
+
+  scenario "users can successfully signup" do
+    visit "/"
+    click_link "Sign In"
+    click_link "Sign up"
+
+    fill_in "Email", with: "newuser@example.com"
+    fill_in "Password", with: "password"
+
+    click_button "Sign up"
+
+    expect(page).to have_content("You have signed in successfully")
+
+    within ".user-info" do
+      expect(page).to have_content("newuser@example.com")
+    end
+  end
+
+  scenario "users can successfully signin" do
+    visit "/"
+    click_link "Sign In"
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password"
+
+    click_button "Sign in"
+
+    expect(page).to have_content("You have signed in successfully")
+
+    within ".user-info" do
+      expect(page).to have_content("test@example.com")
+    end
+  end
+end
