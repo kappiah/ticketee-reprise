3c54425004cecb1924d9ce8b5cce64eeace0d6ea
Kwasi Appiah <kwasi@appiah.id.au>
2015-09-24 01:38:01 +1000

    Don't list projects the user doesn't have permission to see

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 RSpec.feature "Users can view projects" do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:project) { FactoryGirl.create(:project, name: "Sublime Text 3") }
+  let!(:hidden) { FactoryGirl.create(:project, name: "Nothing to see here") }
 
   before do
-    visit root_path as: user
     assign_role!(user, :viewer, project)
+    visit root_path as: user
   end
 
   scenario "with the project details" do
-
     click_link "Sublime Text 3"
     expect(page.current_url).to eq(project_url(project))
+  end
+
+  scenario "unless they have permission to do so" do
+    expect(page).to_not have_content("Nothing to see here")
   end
 end
