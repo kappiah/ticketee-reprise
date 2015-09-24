d1d43e0bdedd72be11413bb3f9609bf8d3ffc0c1
Kwasi Appiah <kwasi@appiah.id.au>
2015-09-24 23:51:46 +1000

    Only members of a project can view its tickets

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -1,68 +1,40 @@
 require 'rails_helper'
 
-describe ProjectPolicy do
+describe TicketPolicy do
 
   let(:user) { User.new }
 
   subject { described_class }
 
-  context "policy_scope" do
-    subject { Pundit.policy_scope(user, Project) }
-
-    let!(:project) { FactoryGirl.create(:project) }
-    let!(:user) { FactoryGirl.create(:user) }
-
-    it "is empty for anonymous users" do
-      expect(Pundit.policy_scope(nil, Project)).to be_empty
-    end
-
-    it "includes projects a user is allowed to view" do
-      assign_role!(user, :viewer, project)
-      expect(subject).to include(project)
-    end
-
-    it "does not contain projects a user is not allowed to view" do
-      expect(subject).to be_empty
-    end
-
-    it "returns all projects for admins" do
-      user.admin = true
-      expect(subject).to include(project)
-    end
-  end
-
   context "permissions" do
-    subject { ProjectPolicy.new(user, project) }
+    subject { TicketPolicy.new(user, ticket) }
 
     let(:user) { FactoryGirl.create(:user) }
     let(:project) { FactoryGirl.create(:project) }
+    let(:ticket) { FactoryGirl.create(:ticket, project: project) }
 
     context "anonymous users" do
       let(:user) { nil }
 
       it { should_not permit_action :show }
-      it { should_not permit_action :update }
     end
 
     context "for viewers of the project" do
       before { assign_role!(user, :viewer, project) }
 
       it { should permit_action :show }
-      it { should_not permit_action :update }
     end
 
     context "for editors of the project" do
       before { assign_role!(user, :editor, project) }
 
       it { should permit_action :show }
-      it { should_not permit_action :update }
     end
 
     context "for managers of the project" do
       before { assign_role!(user, :manager, project) }
 
       it { should permit_action :show }
-      it { should permit_action :edit }
     end
 
     context "for managers of other projects" do
@@ -71,14 +43,12 @@ describe ProjectPolicy do
       end
 
       it { should_not permit_action :show }
-      it { should_not permit_action :update }
     end
 
     context "for administrators" do
       let(:user) { FactoryGirl.create(:user, :admin) }
 
       it { should permit_action :show }
-      it { should permit_action :update }
     end
   end
 end
