a8c2c00cc6cfc5579d267dea2aa7956d94f07305
Kwasi Appiah <kwasi@appiah.id.au>
2016-04-20 00:26:05 +1000

    Default capitalisation has changed...
    Specs failing post upgrade due to the changes in the default
    capitalisation. Adjust specs for now, but should implement i18n..

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -13,7 +13,7 @@ RSpec.feature "Users can edit projects" do
 
   scenario "with valid attributes" do
     fill_in "Name", with: "Sublime Text 4 beta"
-    click_button "Update Project"
+    click_button "Update project"
 
     expect(page).to have_content "Project has been updated"
     expect(page).to have_content "Sublime Text 4 beta"
@@ -22,7 +22,7 @@ RSpec.feature "Users can edit projects" do
   scenario "with invalid attributes" do
     fill_in "Name", with: ""
 
-    click_button "Update Project"
+    click_button "Update project"
     expect(page).to have_content "Project has not been updated"
   end
 end
